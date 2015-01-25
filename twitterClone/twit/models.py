e253b0266388e894bbdf0f2f2f700c361158180d
meera1hahn <meera1hahn@gmail.com>
2015-01-25 15:00:56 -0500

    connecting to port and creating first viewable page

--- twitterClone/twit/models.py
+++ twitterClone/twit/models.py
@@ -4,6 +4,6 @@ class Tweet(models.Model):
 	text = models.CharField(max_length=140)
 	date = models.DateTimeField('date published')
 	name = models.CharField(max_length=12)
-	def __str__ (self):
+	def __unicode__ (self):
 		return self.text 
 	
