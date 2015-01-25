7a8495af3ee4ceeedcac108113dda89e0a47931f
meera1hahn <meera1hahn@gmail.com>
2015-01-25 16:50:52 -0500

    code for displaying all tweets in datebase

--- twitterClone/twit/models.py
+++ twitterClone/twit/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-
 class Tweet(models.Model):
-	text = models.CharField(max_length=140)
-	date = models.DateTimeField('date published')
-	name = models.CharField(max_length=12)
-	def __unicode__ (self):
-		return self.text 
-	
+    text = models.CharField(max_length=140)
+    date = models.DateTimeField('date published')
+    name = models.CharField(max_length=12)
+    def __unicode__ (self):
+        return self.text
