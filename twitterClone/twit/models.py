746637935ca8052718fa1a51151cf07e50c9070c
Andrew Reid Kilgore <akilgo2@dutch.mathcs.emory.edu>
2015-01-25 14:21:25 -0500

    set up database/model

---
+++ twitterClone/twit/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Tweet(models.Model):
+	text = models.CharField(max_length=140)
+	date = models.DateTimeField('date published')
+	name = models.CharField(max_length=12)
+	def __str__ (self):
+		return self.text 
+	
