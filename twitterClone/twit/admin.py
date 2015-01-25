c20367c0f90879d916c820054d03eb324456b8cb
Andrew Reid Kilgore <akilgo2@dutch.mathcs.emory.edu>
2015-01-25 14:35:58 -0500

    more server set up/config

--- twitterClone/twit/admin.py
+++ twitterClone/twit/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from twit.models import Tweet
+
+class TwitAdmin(admin.ModelAdmin):
+	fields = ['text', 'date', 'name']
+
+admin.site.register(Twit, TwitAdmin)
