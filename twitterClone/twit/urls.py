0c25ff3fe059d4a8afad0cb2b73bfac5c4b417b3
meera1hahn <meera1hahn@gmail.com>
2015-01-27 12:21:25 -0500

    "fixing linking error with urls"

--- twitterClone/twit/urls.py
+++ twitterClone/twit/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from twit import views
 
 urlpatterns = patterns('',
-                url(r'^$', views.index, name='index'),
+                url(r'^', views.index, name='index'),
                 url(r'^(?P<twit_id>\d+)/$', views.detail, name='detail'),
                 url(r'^(?P<twit_text>\d+)/(?P<twit_name>\d+)/$', views.tweet, name='tweet'),
                 )
