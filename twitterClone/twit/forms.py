06c3b691278701fd52eeaaf08e044ba52ed4f0cc
meera1hahn <meera1hahn@gmail.com>
2015-01-29 17:26:51 -0500

    fixing changes

--- twitterClone/twit/forms.py
+++ twitterClone/twit/forms.py
@@ -53,7 +53,7 @@ class RegisterForm(forms.ModelForm):
         return password2
 
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
+        user = super(RegisterForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
