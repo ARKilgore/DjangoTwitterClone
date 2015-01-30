44bde0da59ee106ceb9190df0af5c6e4b7634a0d
meera1hahn <meera1hahn@gmail.com>
2015-01-30 13:08:21 -0500

    added error handling for the login form

--- twitterClone/twit/views.py
+++ twitterClone/twit/views.py
@@ -31,24 +31,28 @@ from django.shortcuts import get_object_or_404
 
 
 def index(request):
-    return render(request, 'twit/login.html')
+    context = {}
+    return render(request, 'twit/login.html', context)
 
 def login(request):
+    context = {}
     user = authenticate(username=request.POST.get('user_name',False), password=request.POST.get('password',False))
     if user is not None:
         twit_list = Tweet.objects.order_by('-date')[:10]
         context = { 'twit_list' : twit_list }
         return render(request, 'twit/index.html', context)
     else:
-        return render(request, 'twit/login.html')
+        context['errors'] = 'Username or Password is incorrect. Try again'
+        return render(request, 'twit/login.html', context)
 
 def register(request):
     return render(request, 'twit/registration.html')
 
 def signup(request):
+    context = {}
     user = User.objects.create_user(request.POST.get('Username',False), request.POST.get('Email',False), request.POST.get('Password',False))
     user.save()
-    return render(request, 'twit/login.html')
+    return render(request, 'twit/login.html', context)
 
 '''def signup(request):
     context = {}
